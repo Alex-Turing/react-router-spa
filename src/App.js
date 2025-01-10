746cfee4c666d2d46be9bb3b68761c125130ce28
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-01-10 13:49:38 -0500

    starting SPA implementation using react-router-dom with Link. Connect external data (fake API using json-server). Connect to db.json using axios in component api.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './assets/css/componentes/card.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Page404 from './pages/Page404';
+import Header from './components/Header';
 
 function App() {
 
   
   return (
       <Router>
+        {/**
+         * Todo Aquello que se defina despues de <Router /> y antes de <Routes />
+         * Seran componentes o elmeentos comunes para toda la pagina y permaneceran
+         * sin importar a que ruta vaya el usuario. 
+         */}
+        <Header />
         <Routes>
           <Route path='/' element={ <Home /> } />
           <Route path='about' element={ <About /> } />
+          <Route path='*' element={ <Page404 /> } />
         </Routes>
       </Router>
   );
